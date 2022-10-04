ac93710f49321e498f8841cb75091bf1df3d60ca
Settter <egor-udon@mail.ru>
2022-10-05 00:39:05 +0300

    .

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('')
+@app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('main_page.html')
 
 
 if __name__ == '__main__':
